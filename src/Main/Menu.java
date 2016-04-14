f2f032e270f2d42dc5935893e057acce81e7596b
Luki R Daniel <thekucays@gmail.com>
2016-04-14 14:44:19 +0700

    added Main Menu

---
+++ src/Main/Menu.java
@@ -0,0 +1,28 @@
+package Main;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Menu {
+	Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args){
+		//Menu m = new Menu();
+		//m.buatDatabase("AntarDB");
+		//m.hapusDatabase("AntarDB");
+		//m.tampilListDatabase();
+		//m.buatTabel();
+		//m.hapusTabel();
+		//m.tampilListTabel();
+		
+		//ManipulasiDatabase md = new ManipulasiDatabase();
+		//md.buatDatabase("AntarDB");
+		
+		ManipulasiTabel mt = new ManipulasiTabel();
+		mt.buatTabel();
+	}
+}
