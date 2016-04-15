d57a7cf11c673037363c5b2dd06a85b5635ef2b3
Luki R Daniel <thekucays@gmail.com>
2016-04-15 15:30:24 +0700

    added functionality to delete record

--- src/Main/ManipulasiKueri.java
+++ src/Main/ManipulasiKueri.java
@@ -73,12 +73,51 @@ public class ManipulasiKueri {
 		//System.out.println("Generated sql query :  " + sql);
 	}
 	
-	public void editRecord(){
+	public void editRecord(String Db, String Tbl){
 		
 	}
 	
-	public void hapusRecord(){
+	public void hapusRecord(String Db, String Tbl)throws SQLException{
+		String namaDB = Db;
+		String namaTabel = Tbl;
+		int jumlahkolom = 0;
+		ResultSet rs;
+		ResultSetMetaData rsMeta;
+		DatabaseMetaData dbMeta;
 		
+		// konek ke database yang dimasukin
+		con = kon.konekNamaDB(namaDB);
+		
+		// kueri jumlah kolom yang ada di tabel nya
+		dbMeta = con.getMetaData();
+		rs = dbMeta.getColumns(null, null, namaTabel, null);
+		while(rs.next()){
+			jumlahkolom++;
+		}
+			
+		// simpen nama kolom nya ke array
+		String[] namakoloms = new String[jumlahkolom];
+		int hitung = 0;
+		rs.beforeFirst(); // balikin kursor nya ke awal
+		while(rs.next()){
+			namakoloms[hitung] = rs.getString("COLUMN_NAME");
+			hitung++;
+		}
+		hitung = 0;
+		
+		// tampilin record(isi) tabel nya dulu
+		tampilRecord(namaDB, Tbl);
+		
+		// setelah data tampil, pilih kolom pertama pada tabel untuk dijadiin WHERE nya
+		System.out.println("Masukkan " + namakoloms[0] + " yang ingin dihapus : ");
+		String hapus = scan.nextLine();
+		
+		// generate string sql untuk DELETE nya, lalu eksekusi
+		String sql = "delete from " + namaTabel + " where " + namakoloms[0] + " = '" + hapus + "'";
+		stmt = con.createStatement();
+		stmt.executeUpdate(sql);
+		
+		System.out.println("Record " + hapus +" pada tabel " + namaTabel + " berhasil dihapus");
 	}
 	
 	public void tampilRecord(String Db, String Tbl) throws SQLException{
